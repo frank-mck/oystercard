cb5dd6b07c3cc471e55d6e849b0f1dc5adea27cb
Frank Mckenna <fmckenna56330@gmail.com>
2021-06-01 17:31:26 +0100

    refactored

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -9,7 +9,7 @@ class Oystercard
 
   def top_up(amount)
     @amount = amount
-    fail 'top up exceeds limit' if exceeded?
+    fail "top up of #{@amount} exceeds max limit of #{MAX_BALANCE}" if exceeded?
     @balance += amount
   end
     
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:max_balance) { Oystercard::MAX_BALANCE }
   it 'has initial balance' do
     expect(subject.balance).to eq(0)
   end
@@ -13,13 +14,12 @@ describe Oystercard do
     end
 
     it 'expects a default maximum balance on oystercard' do
-      max_balance = Oystercard::MAX_BALANCE
       expect(max_balance).to eq(90)
     end
 
     it 'raise error if the top up would take the card obove max limit' do
       subject.top_up(90)
-      expect {subject.top_up(1)}.to raise_error('top up exceeds limit')
+      expect {subject.top_up(1)}.to raise_error("top up of 1 exceeds max limit of #{max_balance}")
     end
   end
 end
